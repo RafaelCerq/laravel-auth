6241f8c7b252ebaef4e992adc18d08acab41f607
rafael <rafael.cerqueira@afgbrasil.com.br>
2022-09-21 17:54:30 -0300

    Ajustes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,100 +2,111 @@
 
 @section('content')
 
-    <!--
-    ==================================================
-    Portfolio Section Start
-    ================================================== -->
-    <section id="feature">
+    <!--=============================
+    =            Gallery            =
+    ==============================-->
+    <section id="gallery" class="gallery">
         <div class="container">
-            <div class="section-heading">
-            <h1 class="title wow fadeInDown" data-wow-delay=".3s">Offer From Me</h1>
-            <p class="wow fadeInDown" data-wow-delay=".5s">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed,<br> quasi dolores numquam dolor vero ex, tempora
-                commodi repellendus quod laborum.
-            </p>
-            </div>
-            <div class="row">
-            <div class="col-sm-6 col-lg-4">
-                <div class="media wow fadeInUp animated" data-wow-duration="500ms" data-wow-delay="300ms">
-                <div class="media-left">
-                    <div class="icon">
-                    <i class="ion-ios-flask-outline"></i>
+        <div class="row">
+            <div class="col-md-4 col-sm-6">
+            <figure class="wow fadeInLeft animated portfolio-item animated" data-wow-duration="500ms" data-wow-delay="0ms"
+                style="visibility: visible; animation-duration: 300ms; -webkit-animation-duration: 300ms; animation-delay: 0ms; -webkit-animation-delay: 0ms; animation-name: fadeInLeft; -webkit-animation-name: fadeInLeft;">
+                <div class="img-wrapper">
+                <img src="images/portfolio/item-1.jpg" class="img-fluid" alt="this is a title">
+                <div class="overlay">
+                    <div class="buttons">
+                    <a rel="gallery" class="fancybox" href="images/portfolio/item-1.jpg">Demo</a>
                     </div>
                 </div>
-                <div class="media-body">
-                    <h4 class="media-heading">Media heading</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, sint.</p>
                 </div>
-                </div>
+            </figure>
             </div>
-            <div class="col-sm-6 col-lg-4">
-                <div class="media wow fadeInDown animated" data-wow-duration="500ms" data-wow-delay="600ms">
-                <div class="media-left">
-                    <div class="icon">
-                    <i class="ion-ios-lightbulb-outline"></i>
+            <div class="col-md-4 col-sm-6">
+            <figure class="wow fadeInLeft animated animated" data-wow-duration="500ms" data-wow-delay="300ms"
+                style="visibility: visible; animation-duration: 300ms; -webkit-animation-duration: 300ms; animation-delay: 300ms; -webkit-animation-delay: 300ms; animation-name: fadeInLeft; -webkit-animation-name: fadeInLeft;">
+                <div class="img-wrapper">
+                <img src="images/portfolio/item-2.jpg" class="img-fluid" alt="this is a title">
+                <div class="overlay">
+                    <div class="buttons">
+                    <a rel="gallery" class="fancybox" href="images/portfolio/item-2.jpg">Demo</a>
                     </div>
                 </div>
-                <div class="media-body">
-                    <h4 class="media-heading">Well documented.</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, sint.</p>
                 </div>
-                </div>
+            </figure>
             </div>
-            <div class="col-sm-6 col-lg-4">
-                <div class="media wow fadeInDown animated" data-wow-duration="500ms" data-wow-delay="900ms">
-                <div class="media-left">
-                    <div class="icon">
-                    <i class="ion-ios-lightbulb-outline"></i>
+            <div class="col-md-4 col-sm-6">
+            <figure class="wow fadeInLeft animated animated" data-wow-duration="500ms" data-wow-delay="500ms"
+                style="visibility: visible; animation-duration: 300ms; -webkit-animation-duration: 300ms; animation-delay: 300ms; -webkit-animation-delay: 300ms; animation-name: fadeInLeft; -webkit-animation-name: fadeInLeft;">
+                <div class="img-wrapper">
+                <img src="images/portfolio/item-3.jpg" class="img-fluid" alt="">
+                <div class="overlay">
+                    <div class="buttons">
+                    <a rel="gallery" class="fancybox" href="images/portfolio/item-3.jpg">Demo</a>
                     </div>
                 </div>
-                <div class="media-body">
-                    <h4 class="media-heading">Well documented.</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, sint.</p>
                 </div>
-                </div>
+            </figure>
             </div>
-            <div class="col-sm-6 col-lg-4">
-                <div class="media wow fadeInDown animated" data-wow-duration="500ms" data-wow-delay="1200ms">
-                <div class="media-left">
-                    <div class="icon">
-                    <i class="ion-ios-americanfootball-outline"></i>
+            <div class="col-md-4 col-sm-6">
+            <figure class="wow fadeInLeft animated" data-wow-duration="500ms" data-wow-delay="700ms"
+                style="visibility: hidden; animation-duration: 300ms; -webkit-animation-duration: 300ms; animation-delay: 600ms; -webkit-animation-delay: 600ms; animation-name: none; -webkit-animation-name: none;">
+                <div class="img-wrapper">
+                <img src="images/portfolio/item-4.jpg" class="img-fluid" alt="">
+                <div class="overlay">
+                    <div class="buttons">
+                    <a rel="gallery" class="fancybox" href="images/portfolio/item-4.jpg">Demo</a>
                     </div>
                 </div>
-                <div class="media-body">
-                    <h4 class="media-heading">Free updates</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, sint.</p>
                 </div>
-                </div>
+            </figure>
             </div>
-            <div class="col-sm-6 col-lg-4">
-                <div class="media wow fadeInDown animated" data-wow-duration="500ms" data-wow-delay="1500ms">
-                <div class="media-left">
-                    <div class="icon">
-                    <i class="ion-ios-keypad-outline"></i>
+            <div class="col-md-4 col-sm-6">
+            <figure class="wow fadeInLeft animated" data-wow-duration="500ms" data-wow-delay="900ms"
+                style="visibility: hidden; animation-duration: 300ms; -webkit-animation-duration: 300ms; animation-delay: 900ms; -webkit-animation-delay: 900ms; animation-name: none; -webkit-animation-name: none;">
+                <div class="img-wrapper">
+                <img src="images/portfolio/item-5.jpg" class="img-fluid" alt="">
+                <div class="overlay">
+                    <div class="buttons">
+                    <a rel="gallery" class="fancybox" href="images/portfolio/item-5.jpg">Demo</a>
                     </div>
                 </div>
-                <div class="media-body">
-                    <h4 class="media-heading">Solid Support</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, sint.</p>
                 </div>
-                </div>
+            </figure>
             </div>
-            <div class="col-sm-6 col-lg-4">
-                <div class="media wow fadeInDown animated" data-wow-duration="500ms" data-wow-delay="1800ms">
-                <div class="media-left">
-                    <div class="icon">
-                    <i class="ion-ios-barcode-outline"></i>
+            <div class="col-md-4 col-sm-6">
+            <figure class="wow fadeInLeft animated" data-wow-duration="500ms" data-wow-delay="1100ms"
+                style="visibility: hidden; animation-duration: 300ms; -webkit-animation-duration: 300ms; animation-delay: 1200ms; -webkit-animation-delay: 1200ms; animation-name: none; -webkit-animation-name: none;">
+                <div class="img-wrapper">
+                <img src="images/portfolio/item-6.jpg" class="img-fluid" alt="">
+                <div class="overlay">
+                    <div class="buttons">
+                    <a rel="gallery" class="fancybox" href="images/portfolio/item-6.jpg">Demo</a>
                     </div>
                 </div>
-                <div class="media-body">
-                    <h4 class="media-heading">Simple Installation</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, sint.</p>
                 </div>
-                </div>
-            </div>
+            </figure>
             </div>
         </div>
-    </section> <!-- /#feature -->
+        </div>
+    </section>
+  
+    <!--
+    ==================================================
+    Call To Action Section Start
+    ================================================== -->
+    <section id="call-to-action">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="block">
+                        <h2 class="title wow fadeInDown" data-wow-delay=".3s" data-wow-duration="500ms">SO WHAT YOU THINK ?</h1>
+                        <p class="wow fadeInDown" data-wow-delay=".5s" data-wow-duration="500ms">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis,<br>possimus commodi, fugiat magnam temporibus vero magni recusandae? Dolore, maxime praesentium.</p>
+                        <a href="contact.html" class="btn btn-default btn-contact wow fadeInDown" data-wow-delay=".7s" data-wow-duration="500ms">Contact With Me</a>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </section>
 
 @endsection
