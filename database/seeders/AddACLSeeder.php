61c91f3d840848a1a652a301c901a9d7f397eaba
rafael <rafael.cerqueira@afgbrasil.com.br>
2022-10-20 11:17:04 -0300

    ajustes migration

--- database/seeders/AddACLSeeder.php
+++ database/seeders/AddACLSeeder.php
@@ -29,7 +29,6 @@ class AddACLSeeder extends Seeder
        $showUser = \App\Permission::firstOrCreate(['name'=>'show-user'],['description'=>'Visualizar registro']);
        $deleteUser = \App\Permission::firstOrCreate(['name'=>'delete-user'],['description'=>'Deletar registro']);
        $acessoACL = \App\Permission::firstOrCreate(['name'=>'acl'],['description'=>'Acesso ao ACL']);
-       $acessoACL = \App\Permission::firstOrCreate(['name'=>'manage_bets'],['description'=>'Acesso ao ACL']);
 
 
        echo "Registro de ACL criados! \n";
